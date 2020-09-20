7c8d2439922ea5d136f7e9a61e7890c3a94fd2b4
manish-VAIO\manish <sarishtijain1997@gmail.com>
2020-09-21 00:52:56 +0530

    Initial commit
    movie-catalog-service

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,31 @@
+package io.javabrains.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+	}
+
+	@Bean
+	public WebClient.Builder getWebClientBuilder() {
+		/*
+		 * WebClient.Builder builder = WebClient.builder(); return builder;
+		 */
+		return WebClient.builder();
+	}
+
+}
